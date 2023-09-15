90aef971b1efd6dd66659cfdbe1c6c748b562ae0
Mariusz-72 <mmbwdap@gmail.com>
2023-09-15 21:03:21 +0200

    update 2.1 task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+import SimpleLightbox from "simplelightbox"; 
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+galleryList.style.listStyle = 'none';  //usunięcie kropek z listy//
+
+const createGalleryItem = ({ preview, original, description }) => {
+  return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+    </li>
+    `;
+
+};
+
+//galeria z danymi z tablicy galleryItems //
+const galleryMarkup = galleryItems
+    .map(createGalleryItem)
+    .join('');
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+//uruchomienie biblioteki SimpleLightbox//
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
+ const iframe = document.querySelector('iframe');
+ const player = new Vimeo.Player(iframe);
+
+ player.on('play', function () {
+   console.log('played the video!');
+ });
+
+ player.getVideoTitle().then(function (title) {
+   console.log('title:', title);
+ });
+
+const timeUpdate = throttle(data => {
+    localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+}, 1000);
+
+player.on('loaded', () => {
+    const savedTimeUpdate = localStorage.getItem(LOCALSTORAGE_KEY);
+    if (savedTimeUpdate) {
+        player
+            .setCurrentTime(Number.parseFloat(savedTimeUpdate))
+            .catch(function (error) {
+                switch (error.name) {
+                    case 'RangeError':
+                        break;
+                    default:
+                        break;
+                }
+            });
+    }
+});
