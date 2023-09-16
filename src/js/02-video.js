37a970bbe9a4701c398954492636855ab00a0c7a
Mariusz-72 <mmbwdap@gmail.com>
2023-09-16 16:41:40 +0200

    update 3.0

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,18 @@
-import Player from "@vimeo/player";
+
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const LOCALSTORAGE_KEY = 'player-current-time';
 
- player.on('play', function () {
-   console.log('played the video!');
- });
+    player.on('play', function () {
+    console.log('played the video!');
+});
 
- player.getVideoTitle().then(function (title) {
-   console.log('title:', title);
- });
+    player.getVideoTitle().then(function (title) {
+    console.log('title:', title);
+});
 
 const timeUpdate = throttle(data => {
     localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
