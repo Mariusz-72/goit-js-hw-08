c23831aa74807859b9701f5218ec3a6338d88f58
Mariusz-72 <mmbwdap@gmail.com>
2023-09-17 16:46:08 +0200

    Update 4.0
    przeróbka kodu od nowa, usunięcie błędów , sprawdzenie poprawności działania

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,51 +2,53 @@
 'use strict';
 import throttle from 'lodash.throttle';  //import modułu lodash//
 
-const actualData = 'feedback-from-state';  //klucz do localStorage//
+const LOCALSTORAGE_KEY = 'feedback-form-state';  
 
-const feedbackForm = document.querySelector('.feedback-form');
-const emailInput = feedbackForm.querySelector('input [name="email"]');
-const messageTextarea = feedbackForm.querySelector('textarea[name="message"]');
+const form = document.querySelector('.feedback-form');
 
-
-
-const saveDataToLocoalStorage = throttle(() => {
-    const formData = {
-        email: emailInput.value,
-        message: messageTextarea.value,
-    };
-    localStorage.setItem(localStorageKey, JSON.stringify(formData));
-}, 500);
-
-emailInput.addEventListener('input', saveDataToLocoalStorage);
-messageTextarea.addEventListener('input', saveDataToLocoalStorage);
-
-
-function fillFormFromLocalStorage() {
-    const savedData = localStorage.getItem(localStorageKey);
-
-    if (savedData) {
-        const formData = JSON.parse(savedData);
-        emailInput.value = formData.email;
-        messageTextarea.value = formData.message;
-    }
-}
-
-window.addEventListener('load', fillFromLocalStorage);
-
-//utworzenie formData z danymi z pól formularza ://
-const formData = {
-    email: emailInput.value,
-    message: messageText.value,
+let formState = {
+    email: '',
+    message: ''
 };
 
-console.log('Dane z formularza:', formData); // wyświetli dane jakie idą do localStorage//
- 
-localStorage.removeItem(localStorageKey); //czyszczenie localStorage po wysłaniu formularza//
+const checkStorage = localStorage.getItem(LOCALSTORAGE_KEY);
+ if (checkStorage) {
+    formState = JSON.parse(checkStorage);
+        form.elements.email.value = formState.email;
+        form.elements.message.value = formState.message;
+    }
 
-function clearLocalStorage() {
-    localStorage.removeItem(localStorageKey);
+
+const throttledSave = throttle((data) => {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+    
+}, 500);
+
+
+
+
+function saveData(event) {
+    formState[event.target.name] = event.target.value;
+    throttledSave(formState)
 }
 
-window.addEventListener('load', clearLocalStorage);
+form.addEventListener("input", saveData);
 
+const sentForm = eventSent => {
+    eventSent.preventDefault();
+    const {
+        elements: { email, message } } = eventSent.currentTarget;
+    const objectData = {
+        email: email.value,
+        message: message.value,
+    };
+    console.log(objectData);
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    formState = {
+        email: '',
+        message: ''
+    };
+    form.reset();
+};
+
+form.addEventListener('submit', sentForm);
